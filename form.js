c3f03a1a3f75724da32e50a6c06dd38334de6db6
Izaz-ur-Rahman <izazurrahman314@gmail.com>
2024-11-12 15:26:49 +0500

    again uploading

---
+++ form.js
@@ -0,0 +1,3 @@
+var modal=document.getElementById('myModal');var btn=document.querySelector('.scroll');var span=document.getElementsByClassName("close")[0];function openModal(){modal.style.display="block"}
+function closeModal(){modal.style.display="none"}
+window.onclick=function(event){if(event.target==modal){modal.style.display="none"}}
